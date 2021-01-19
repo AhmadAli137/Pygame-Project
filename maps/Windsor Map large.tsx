8a7d454715963484ec201dceed69b0a90866168a
Ahmad Ali <32439767+AhmadAli137@users.noreply.github.com>
2021-01-19 12:30:10 -0700

    Add files via upload

---
+++ maps/Windsor Map large.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Windsor Map (tmx)" tilewidth="64" tileheight="64" tilecount="2000" columns="50">
+ <image source="Windsor Map (tmx).png" width="3200" height="2560"/>
+</tileset>
